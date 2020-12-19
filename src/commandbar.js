066c0a63d540ef676f548b0deae535bed740022b
Noah Krossa <noahkrossa@gmail.com>
2020-12-19 19:35:28 +0100

    Positionate correctly commandsbar, when select a text

--- src/commandbar.js
+++ src/commandbar.js
@@ -1,4 +1,11 @@
 const commansdbar = document.getElementById("commandsbar");
+
+const setCommandsbarPos = (top, left) => {
+  if (left < 0) left = 0;
+  commansdbar.style.left = left + "px";
+  commansdbar.style.top = top + "px";
+};
+
 export const updateConfigBar = (btn, state) => {
   console.log(btn);
   btn.dataset.state = `${state}`;
@@ -9,7 +16,15 @@ export const showCommandbar = () => {
   if (selection) {
     const range = selection.getRangeAt(0);
     const sRect = range.getBoundingClientRect(range);
-    if (sRect.width) commansdbar.style.display = "table";
-    else commansdbar.style.display = "none";
+    if (!sRect.width) {
+      commansdbar.style.display = "none";
+      return;
+    } else {
+      commansdbar.style.display = "table";
+      let top = sRect.top - commansdbar.offsetHeight - 10;
+      let left =
+        sRect.left + sRect.width / 2 - commansdbar.offsetWidth / 2;
+      setCommandsbarPos(top, left);
+    }
   }
 };
